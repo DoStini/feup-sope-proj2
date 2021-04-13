5cead9e95571b6dfac1268139bc2f2a85c7ad215
Andre Moreira <andre.moreira.9@hotmail.com>
2021-04-13 10:37:12 +0000

    Add parser

--- client/src/args_parser.c
+++ client/src/args_parser.c
@@ -1,1 +1,30 @@
 #include "../include/args_parser.h"
+
+#include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int parse_args(int argc, char* argv[], args_data_t* data) {
+    if (argc != 4) {
+        return ERROR;
+    }
+
+    for (size_t i = 0; i < argc; i++) {
+        if (!strcmp(argv[i], "-t")) {
+            if (i == argc - 1)
+                return ERROR;
+
+            int64_t time;
+            if ((time = atoll(argv[i + 1])) <= 0)
+                return ERROR;
+            data->duration = time;
+            i++;
+            continue;
+        }
+
+        data->fifoname = argv[i];
+    }
+
+    return 0;
+}
--- client/src/main.c
+++ client/src/main.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 
 #include "../include/args_parser.h"
+#include "../include/error/exit_codes.h"
 
 int main(int argc, char* argv[], char* envp[]) {
-    printf("%s\n", USAGE);
+    args_data_t data;
+    if (parse_args(argc, argv, &data)) {
+        fprintf(stderr, "%s\n", USAGE);
+        exit(BAD_ARGS);
+    }
+    printf("Duration: %lu\nFifoname: %s\n", data.duration, data.fifoname);
 }
