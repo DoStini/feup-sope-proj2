9da421123a2557e04a32b615ea10b73e46ec9950
Nuno Alves <nyansword@gmail.com>
2021-04-19 16:26:24 +0100

    add: create and join threads

--- client/include/task_creator.h
+++ client/include/task_creator.h
@@ -1,6 +1,9 @@
 #ifndef CLIENT_INCLUDE_TASK_CREATOR_H_
 #define CLIENT_INCLUDE_TASK_CREATOR_H_
 
+#define TASK_CREATOR_ERROR -1
+#define TASK_CREATOR_THREAD_ERROR -1
+
 #include "../include/args_parser.h"
 
 int task_creator(const args_data_t* const data);
--- client/src/task_creator.c
+++ client/src/task_creator.c
@@ -20,28 +20,42 @@ uint64_t get_random_ms(uint64_t lower, uint64_t upper) {
     return (rand_r(&seedp) % (upper - lower) + lower) * MS_TO_NS;
 }
 
-int __tmp__do_nothing() {
+void* __tmp__do_nothing() {
     sleep(rand_r(&seedp) % 2);
-    return 0;
+    printf("%ld says hi and goodbye.\n", pthread_self());
+
+    return NULL;
 }
 
 int create_task() { return 0; }
 
 int task_creator(const args_data_t* const data) {
     struct timespec tspec;
+    array_value_t thread;
     tspec.tv_sec = 0;
     int __tmp__tries = 0;
+    size_t threads_size = 0;
 
     block_array_t* threads = block_array_create(THREAD_VAL, 10);
-    if (threads == NULL) return -1;
+    if (threads == NULL) return TASK_CREATOR_ERROR;
 
     while (__tmp__tries < 10) {
         tspec.tv_nsec = get_random_ms(MIN_WAIT_MS, MAX_WAIT_MS);
         nanosleep(&tspec, NULL);
 
-        // if(pthread_create())
+        if (pthread_create(&thread.thread_value, NULL, __tmp__do_nothing,
+                           NULL)) {
+            return TASK_CREATOR_THREAD_ERROR;
+        }
+        block_array_set(threads, threads_size, thread);
 
+        threads_size++;
         __tmp__tries++;
+    }
+
+    for (size_t i = 0; i < threads_size; i++) {
+        if (block_array_at(threads, i, &thread)) return TASK_CREATOR_ERROR;
+        pthread_join(thread.thread_value, NULL);
     }
 
     block_array_delete(threads);
