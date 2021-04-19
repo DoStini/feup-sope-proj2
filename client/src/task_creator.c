ce0ddb2b5d7d75f0e592258aa1310c1b96e05ba6
Nuno Alves <nyansword@gmail.com>
2021-04-19 18:41:21 +0100

    fix: valgrind errors

--- client/src/task_creator.c
+++ client/src/task_creator.c
@@ -7,6 +7,7 @@
 #include <signal.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "../include/block_array.h"
 
@@ -37,10 +38,14 @@ int start_timer(uint64_t seconds) {
     struct itimerspec spec;
     struct timespec timer_interval;
     struct timespec timer_value;
+
     timer_value.tv_nsec = 0;
     timer_value.tv_sec = seconds;
     timer_interval.tv_sec = timer_interval.tv_nsec = 0;
+
+    memset(&sevp, 0, sizeof(sevp));
     sevp.sigev_notify = SIGEV_NONE;
+
     spec.it_interval = timer_interval;
     spec.it_value = timer_value;
 
