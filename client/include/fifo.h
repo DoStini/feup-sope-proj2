7874269ba29e04d5ba8bb08b3e35d7120afdffbe
Nuno Costa <up201906272@fe.up.pt>
2021-04-28 21:12:04 +0100

    Add remaining documentation

--- client/include/fifo.h
+++ client/include/fifo.h
@@ -7,20 +7,63 @@
 #define MAX_FIFO_NAME FILENAME_MAX
 #define TIMEOUT 5
 
+/**
+ * @brief Create the thread private fifo
+ * 
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int create_private_fifo(void);
 
+/**
+ * @brief Deletes the thread private fifo
+ * 
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int remove_private_fifo(void);
 
+/**
+ * @brief Opens the private fifo for reading
+ * 
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int open_read_private_fifo(void);
 
+/**
+ * @brief Opens the public fifo for writing
+ * 
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int open_write_public_fifo(void);
 
+/**
+ * @brief Set the public fifo file descriptor
+ * 
+ * @param[in] fd the public fifo fd
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int set_public_fifo(int fd);
 
+/**
+ * @brief Gets the public fifo fd
+ * 
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int get_public_fifo();
 
+/**
+ * @brief Waits until the public fifo is open and available to read. 
+ * Initializes other important variables.
+ * 
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int wait_public_fifo();
 
+/**
+ * @brief Closes a fifo given its file descriptor
+ * 
+ * @param[in] fd the given file descriptor
+ * @return int 0 when closed, other when open (file descriptor)
+ */
 int close_fifo(int fd);
 
 #endif  // CLIENT_INCLUDE_FIFO_H_
