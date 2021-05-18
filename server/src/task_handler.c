8fd6267324a4470492af565ffd4084a35cd18c14
Nuno Alves <nyansword@gmail.com>
2021-05-18 12:23:18 +0100

    fix: destroying queue after all threads end

--- server/src/task_handler.c
+++ server/src/task_handler.c
@@ -21,7 +21,7 @@
 
 static unsigned int seedp;
 
-static queue_t *data_queue;
+static queue_t* data_queue = NULL;
 
 static pthread_mutex_t mutex;
 
@@ -31,7 +31,7 @@ uint64_t get_random_ms(uint64_t lower, uint64_t upper) {
 
 int get_random_task() { return rand_r(&seedp) % 9 + 1; }
 
-void *cleanup_thread() {
+void* cleanup_thread() {
     pthread_exit(NULL);
     return NULL;
 }
@@ -40,10 +40,12 @@ void cleanup(void) {
     close_fifo(get_public_fifo());
     int err = remove_public_fifo();
     fprintf(stderr, "UNLINK: %d\n", err);
+    
+    if(data_queue != NULL) queue_destroy(data_queue);
 }
 
-void *producer_handler(void *ptr) {
-    message_t msg = *((message_t *)ptr);
+void* producer_handler(void* ptr) {
+    message_t msg = *((message_t*)ptr);
     free(ptr);
 
     write_log(RECVD, &msg);
@@ -54,6 +56,8 @@ void *producer_handler(void *ptr) {
     if (is_timeout()) {
         err_log("task timedout");
         msg.tskres = -1;
+
+        return cleanup_thread();
     } else {
         msg.tskres = res;
         write_log(TSKEX, &msg);
@@ -62,7 +66,7 @@ void *producer_handler(void *ptr) {
     err_log("mutex queue lock");
     pthread_mutex_lock(&mutex);
 
-    if (!queue_empty(data_queue)) {
+    if (data_queue != NULL && !queue_empty(data_queue)) {
         queue_push(data_queue, &msg);
         queue_print(data_queue);
     }
@@ -73,7 +77,7 @@ void *producer_handler(void *ptr) {
     return cleanup_thread();
 }
 
-void *consumer_handler() {
+void* consumer_handler() {
     // while (!is_timeout()) {
     //     message_t msg;
     //     printf("hello\n");
@@ -95,15 +99,13 @@ void *consumer_handler() {
     return cleanup_thread();
 }
 
-int task_handler(args_data_t *args) {
-    if (create_public_fifo() != 0)
-        return CANT_CREATE_FIFO;
+int task_handler(args_data_t* args) {
+    if (create_public_fifo() != 0) return CANT_CREATE_FIFO;
     atexit(cleanup);
 
     int fd = open_read_public_fifo();
 
-    if (fd < 0)
-        return ERROR;
+    if (fd < 0) return ERROR;
 
     pthread_t consumer_thread;
 
@@ -119,10 +121,10 @@ int task_handler(args_data_t *args) {
     data_queue = queue_(args->buffer_size);
 
     while (!is_timeout()) {
-        pthread_t consumer;
-        message_t *msg = malloc(sizeof(message_t));
+        pthread_t producer_thread;
+        message_t* msg = malloc(sizeof(message_t));
         if (recv_message(msg) == 0) {
-            if (pthread_create(&consumer, NULL, producer_handler, msg)) {
+            if (pthread_create(&producer_thread, NULL, producer_handler, msg)) {
                 free(msg);
                 queue_destroy(data_queue);
                 return TASK_CREATOR_THREAD_ERROR;
@@ -137,8 +139,6 @@ int task_handler(args_data_t *args) {
     pthread_mutexattr_destroy(&mattr);
 
     pthread_join(consumer_thread, NULL);
-
-    queue_destroy(data_queue);
 
     return 0;
 }
