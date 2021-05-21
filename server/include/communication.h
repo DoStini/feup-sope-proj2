4fab95d7400e9101485947161d348d8c9812cc64
Nuno Alves <nyansword@gmail.com>
2021-05-21 13:03:38 +0100

    cleanup: communication

--- server/include/communication.h
+++ server/include/communication.h
@@ -2,26 +2,14 @@
 #define SERVER_INCLUDE_COMMUNICATION_H_
 
 #include <pthread.h>
-#include <sys/types.h>
 
-#include "../../common/common.h"
+#include "common/common.h"
 
 /**
  * @brief Message information (either sent or received)
  *
  */
 typedef Message message_t;
-
-/**
- * @brief Builds a message object
- *
- * @param[out] msg the built message
- * @param[in] id the id of the message
- * @param[in] res the result of the message
- * @param[in] t the time of the message
- * @return int 0 upon sucess, other upon error
- */
-int build_message(message_t* msg, int id, int res, int t);
 
 /**
  * @brief Receives a message from the public fifo
@@ -32,10 +20,12 @@ int build_message(message_t* msg, int id, int res, int t);
 int recv_message(message_t* msg);
 
 /**
- * @brief Receives a message of the thread private fifo
+ * @brief Sends a message to the thread private fifo
  *
- * @param[in] msg the message to send
- * @return int 0 when closed, other when open
+ * @param[in] msg the message to send.
+ * @param pid the client's process id.
+ * @param tid the id of the client's thread that made the request.
+ * @return int 0 when message was sent correctly, otherwise an error is returned.
  */
 int send_private_message(message_t* msg, pid_t pid, pthread_t tid);
 
--- server/src/communication.c
+++ server/src/communication.c
@@ -2,23 +2,10 @@
 
 #include <fcntl.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 #include "include/error/exit_codes.h"
 #include "include/fifo.h"
 #include "include/timer.h"
-
-int build_message(message_t* msg, int id, int res, int t) {
-    memset(msg, 0, sizeof(message_t));
-
-    msg->rid = id;
-    msg->pid = getpid();
-    msg->tskres = res;
-    msg->tskload = t;
-    msg->tid = pthread_self();
-    return 0;
-}
 
 int recv_message(message_t* msg) {
     int fd = get_public_fifo();
@@ -36,26 +23,28 @@ int recv_message(message_t* msg) {
 
     if (err == -1) {
         close(fd);
+
         return ERROR;
-        perror("select()");
     } else if (err) {
         size_t data_size = read(fd, msg, sizeof(message_t));
 
         return sizeof(message_t) == data_size ? 0 : ERROR;
     }
+
     return ERROR;
 }
 
 int send_private_message(message_t* msg, pid_t pid, pthread_t tid) {
     char fifo[MAX_FIFO_NAME] = "";
     snprintf(fifo, MAX_FIFO_NAME, "/tmp/%d.%lu", pid, tid);
-    fprintf(stderr, "[server] opened %s\n", fifo);
+
     int fd = open(fifo, O_RDWR);
     if (fd < 0) return ERROR;
-    perror("private fifo");
-    fprintf(stderr, "open fd: %d\n", fd);
+    fprintf(stderr, "[server] opened %s\n", fifo);
+
     fd_set fds;
     struct timeval timer;
+
     timer_get_remaining_timeval(&timer);
     timer.tv_sec++;
 
@@ -65,21 +54,19 @@ int send_private_message(message_t* msg, pid_t pid, pthread_t tid) {
     int err;
     err = select(fd + 1, NULL, &fds, NULL, &timer);
 
-    if (err == -1) {
-        close(fd);
-        perror("select()");
-        return ERROR;
-    } else if (err) {
+    if (err != 0) {
         int sent_size = write(fd, msg, sizeof(message_t));
 
-        fprintf(stderr, "%d size sent: %d %lu\n", msg->rid, sent_size,
-                sizeof(message_t));
+        // fprintf(stderr, "[server] %d size sent: %d %lu\n", msg->rid,
+        // sent_size,
+        //        sizeof(message_t));
 
         int err = close_fifo(fd);
 
         if (sent_size != sizeof(message_t) || err) return ERROR;
         return 0;
     }
+
     close(fd);
     return ERROR;
 }
